9af667c9fb15d62e4b82ceaa62d845ebe7e6c8de
Lakshman <laxmannani960@gmail.com>
2026-03-29 15:13:39 +0530

    callbacks

--- Javascript/callstack.js
+++ Javascript/callstack.js
@@ -1,13 +1,74 @@
-console.log("start");
+// console.log("start");
 
-setTimeout(() => {
-  console.log("First call");
-}, 5000);
+// setTimeout(() => {
+//   console.log("First call");
+// }, 5000);
 
-for (let i = 0; i < 50000; i++) {
-  console.log("i");
+// for (let i = 0; i < 50000; i++) {
+//   console.log("i");
+// }
+
+// setTimeout(() => {
+//   console.log("second call");
+// }, 3000);
+
+const processNumber = (num, callback) => {
+  const result = num * 2;
+  return callback(result);
+};
+
+processNumber(5, (result) => {
+  console.log("Result:", result);
+});
+
+function forEachElement(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i]);
+  }
+}
+let arr = [1, 2, 3, 4, 5, 6];
+forEachElement(arr, (res) => {
+  console.log("arr", res + 5);
+});
+
+function sayHello(name, callback) {
+  setTimeout(() => {
+    callback(`hello ${name}`);
+  }, 1000);
+}
+sayHello("Lakshman", (callback) => {
+  console.log(callback);
+});
+
+function checkEven(num, evencall, oddcall) {
+  if (num % 2 == 0) {
+    evencall(num);
+  } else {
+    oddcall(num);
+  }
 }
 
-setTimeout(() => {
-  console.log("second call");
-}, 3000);
+checkEven(
+  21,
+  (evencall) => {
+    console.log("Even number:", evencall);
+  },
+  (oddcall) => {
+    console.log("Odd number:", oddcall);
+  },
+);
+
+function performTasks(num, add, multiply) {
+  num = add(num);
+  num = multiply(num);
+  console.log(num);
+}
+performTasks(
+  10,
+  (add) => {
+    return (add += 5);
+  },
+  (multiply) => {
+    return (multiply *= 2);
+  },
+);
